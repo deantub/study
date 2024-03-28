758b4c7ff01a62cf5fb611e1546dee91eae49e8e
Dean <138621406+deantub@users.noreply.github.com>
2024-03-28 19:04:51 +0200

    color map

--- mpl_squares.py
+++ mpl_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-pastel')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c='red', s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Задати назву для графіка та кожної з осей.
 ax.set_title("Square Numbers", fontsize=24)
@@ -18,4 +18,4 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 # Задати діапазон для кожної з осей.
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
