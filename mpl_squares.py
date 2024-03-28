f7adcf4d4d39a71d62dbe8d2f2eccff44a00b8cd
Dean <138621406+deantub@users.noreply.github.com>
2024-03-28 18:58:36 +0200

    dot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,11 +1,11 @@
 import matlotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-pastel')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, c='red', s=10)
 
 # Задати назву для графіка та кожної з осей.
 ax.set_title("Square Numbers", fontsize=24)
@@ -14,5 +14,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Задати розмір шрифту для підписів на розмітці.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Задати діапазон для кожної з осей.
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
