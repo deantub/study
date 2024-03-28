721eca5023e17b22a8267f032dbfb7550230a995
Dean <138621406+deantub@users.noreply.github.com>
2024-03-28 18:17:45 +0200

    input values

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,10 @@
 import matlotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Задати назву для графіка та кожної з осей.
 ax.set_title("Square Numbers", fontsize=24)
