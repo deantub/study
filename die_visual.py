086d4b272f70cd2a50cb625c4bb904b7e6e9d56b
Dean <138621406+deantub@users.noreply.github.com>
2024-04-01 13:10:32 +0300

    two dies

--- die_visual.py
+++ die_visual.py
@@ -3,13 +3,13 @@ from plotly import offline
 
 from die import Die
 
-# Створити два кубики D6
+# Створити два кубики D6 та D10.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Зробити декілька кидків та зберегти результати у список
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -26,5 +26,5 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+my_layout = Layout(title='Results of rolling a D6 and D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
