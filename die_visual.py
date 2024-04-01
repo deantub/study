f9ffa5ec08394d33f8f9051a1bf00e5975f74ed7
Dean <138621406+deantub@users.noreply.github.com>
2024-04-01 13:01:54 +0300

    two die

--- die_visual.py
+++ die_visual.py
@@ -1,12 +1,30 @@
+from plotly.gragh_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
-# Створити D6
-die = Die()
+# Створити два кубики D6
+die_1 = Die()
+die_2 = Die()
 
 # Зробити декілька кидків та зберегти результати у список
 results = []
-for roll_num in range(100):
-    result = die.roll()
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
-print(results)
+# Проаналізувати результати
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Візуалізувати результати.
+x_values = list(range(2, max_result+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result', 'dtick': 1}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
